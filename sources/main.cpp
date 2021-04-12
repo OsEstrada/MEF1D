b39e694cdc205ff4f6d916d84c308042c3b2a5b5
OsEstrada <00064318@uca.edu.sv>
2021-04-12 11:34:52 -0600

    fifth commit

--- sources/main.cpp
+++ sources/main.cpp
@@ -10,39 +10,40 @@ int main(int argc, char const *argv[])
 {
     vector<Matrix> localKs;
     vector<Vector> localbs;
-.
+
     Matrix K;
     Vector b;
     Vector T;
 
-    cout << "IMPLEMENTACI" << char(224) << "N DEL M" << char(144) << "TODO DE LOS ELEMENTOS FINITOS\n"
+    cout << "IMPLEMENTACION DEL METODO DE LOS ELEMENTOS FINITOS\n"
             << "\t- TRANSFERENCIA DE CALOR\n"
-            << "\t- 1 DIMENSI" << char(224) << "N\n"
+            << "\t- 1 DIMENSION\n"
             << "\t- FUNCIONES DE FORMA LINEALES\n"
             << "\t- PESOS DE GALERKIN\n"
-            << "*********************************************************************************\n\n";
+            << "*********************************************************************************"<<endl<<endl;
 
     mesh m;
     leerMallayCondiciones(m);
 
     crearSistemasLocales(m, localKs, localbs);
-    //showKs(localKs); showbs(localbs);
+    showKs(localKs); showbs(localbs);
 
     zeroes(K, m.getSize(NODES));
     zeroes(b, m.getSize(NODES));
     ensamblaje(m, localKs, localbs, K, b);
-    //showMatrix(K); showVector(b);
+    showMatrix(K); showVector(b);
 
     applyNeumann(m, b);
-    //showVector(b);
+    showVector(b);
 
     applyDirichlet(m, K, b);
-    //showMatrix(K); showVector(b);
+    showMatrix(K); showVector(b);
 
     zeroes(T, b.size());
     calculate(K, b, T);
 
-    cout << "La respuesta es: \n";
+    cout << "La respuesta es:"<<endl;
     showVector(T);
+
     return 0;
 }
